b6cd96b48da3393a002b686b682f3fc65f24b8cc
AllysonOliveiraSouza <131881163+AllysonOliveiraSouza@users.noreply.github.com>
2024-04-22 22:03:20 -0300

    Projeto 90% concluído

---
+++ msg.js
@@ -0,0 +1,3 @@
+const btnContato = document.querySelector('.btn-contato');
+let apertarBotao = () => alert('Entre em contato comigo no whatsapp (11)95435-7910');
+btnContato.addEventListener('click', apertarBotao);
